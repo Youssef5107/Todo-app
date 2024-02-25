82e160ab6cfd1d5781c2cf51492357abe69880f6
Youssef <ybelalmarzouk@gmail.com>
2024-02-25 10:58:51 +0200

    feat:add local storage

--- javascript.js
+++ javascript.js
@@ -1,12 +1,25 @@
 var input = document.querySelector(".task-input");
-var tableContent = "";
+var tableContent = localStorage.getItem("tasks") || "";
+var table = document.querySelector("tbody");
+
+if (table) {
+	table.innerHTML = tableContent || `<tr>No tasks</tr>`;
+}
 
 input.addEventListener("keydown", function (pressedKey) {
-	var table = document.querySelector("tbody");
-
 	if (pressedKey.key === "Enter") {
-		tableContent = tableContent + `<tr>${input.value}</tr>`
+		var newRow = `
+      <tr>
+        <td>
+          <input type="checkbox" class="checkbox" name="" id="">
+          ${input.value}
+        </td>
+      </tr>
+    `;
+
+		tableContent += newRow;
 		table.innerHTML = tableContent;
+		localStorage.setItem("tasks", tableContent);
 		input.value = "";
 	}
 });
